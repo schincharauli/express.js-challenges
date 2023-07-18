75c54504dccd326930eaac322ccf8871c667f782
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-07-18 21:29:54 +0400

    server made

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello World");
+// console.log("Hello World");
 
 module.exports = app;
+
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+console.log(app);
