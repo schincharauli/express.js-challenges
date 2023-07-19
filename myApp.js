530732510f1c0c65396bcf14cb06a502c21a4a85
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-07-19 09:14:10 +0400

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -16,3 +16,5 @@ absolutePath = __dirname + "/views/index.html";
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app.use("/public", express.static(__dirname + "/public"));
